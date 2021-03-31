5a89a5579b4d30e52d6fa06009d17eaec560dcde
parmeet <2020a1e080@mietjammu.in>
2021-04-01 00:02:45 +0530

    Program to check if a number is even or odd.

---
+++ lab07.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+
+{
+
+ int n;   
+ printf("Enter the number:\n");
+ scanf("%d",&n);
+
+ if(n%2==0)
+ {
+  printf("It is Even\n");
+ }
+ else
+ {
+  printf("It is Odd\n");
+ }
+
+ return 0;
+ 
+}
