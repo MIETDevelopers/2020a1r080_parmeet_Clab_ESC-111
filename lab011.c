eda4d78da9d032cf823e59f07c6ccbeea8b48969
parmeet <2020a1e080@mietjammu.in>
2021-04-02 09:48:42 +0530

    Program to print the Fibonacci sequence using for loop.

---
+++ lab011.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+ int main ()
+
+{
+ 
+    int n1 = 0, n2 = 1, n3, i, num;
+    printf ("Enter the num of terms;\n");
+    scanf ("%d", &num);
+    printf ("The Fibonacci series is :\n");
+    printf ("%d%d", n1, n2);
+    for (i=2; i<=num; ++i)
+ 
+    {
+ 
+      n3 = n1 + n2;
+      printf ("%d ", n3);
+      n1 = n2;
+      n2 = n3;
+ 
+    }
+ 
+  return 0;
+ 
+}
