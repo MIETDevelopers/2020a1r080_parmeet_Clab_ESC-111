d9e65c329f2c441d1d589bb31392124d84c907c2
parmeet <2020a1e080@mietjammu.in>
2021-04-06 01:03:34 +0530

    Program to demonstrate the use of various string operations.

---
+++ lab21.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char string[100];
+    printf("Write something:\n");
+    scanf("%s", string);
+
+    printf("The string is : %s\n", string);
+    return 0;
+}
